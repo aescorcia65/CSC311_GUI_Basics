3f8a4839317a7151390400bde4d6ba1620b88e76
andrew <drew.escorcia@gmail.com>
2025-02-17 12:51:13 -0500

    init

---
+++ src/main/java/org/example/csc311_gui_basics/HelloController.java
@@ -0,0 +1,16 @@
+package org.example.csc311_gui_basics;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Ax2 Technologies");
+    }
+
+}
